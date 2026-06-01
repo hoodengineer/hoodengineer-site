f20c8b8031d64d59e1b1de9122ccf44ae1875742
hoodengineer <hoodengineer2026@gmail.com>
2026-06-01 08:55:51 -0400

    add first post

--- blog.js
+++ blog.js
@@ -1,1 +1,24 @@
 const blogTitle = "Thoughts"
+
+const post = {
+    title: 'New Territory: Javascript',
+    date: '6-1-26',
+    tag: 'Project',
+    body: ` The whole purpose of this expense tracker is to allow myself to see a visual representation by way of a reactive line graph.
+
+    This being my first actual project, I plan to take things very slow. Personally, I dont want to rush to push commits for the hell of it.
+
+    I mean when you get into a new relationship asking shawty if she's ready to committ off-the-rip is hella aggressive. I want to spend time with javascript for over a year while documenting all of it.
+    
+    I think it makes the process more interesting as I see my thought pattern. For readers it also give you access to how I think about problems, and I'd like
+    
+    feedback from you also to help me on this journey. 
+    
+    I'm currently learning while I build. I believe that approach may have a better ROI. It beats the whole syntax tutorials thing.
+    
+    Right now I'm setting up the current page you are reading this on, so that I can blog simultaneously. 
+    
+    Thanks for reading my g's!`
+
+
+};
